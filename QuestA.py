86b7b58687ebd28f6cacf54d164fc6ca7af6b3eb
NeziaS <art15_01@mail.ru>
2024-04-01 14:46:51 +0300

    progg

--- QuestA.py
+++ QuestA.py
@@ -5,5 +5,11 @@ q=-1000000000000000
 for i in range(len(a)):
     if a[i]>q and a[i]%100==17:
         q=a[i]
+w=0
+e=-10000000000000000
 for i in range(len(a)-2):
-    if
+    if (a[i]+a[i+1]+a[i+2])>q and ((len(str(a[i]))==4 and len(str(a[i+1]))==4 and len(str(a[i+2]))!=4) or (len(str(a[i]))==4 and len(str(a[i+1]))!=4 and len(str(a[i+2]))==4) or (len(str(a[i]))!=4 and len(str(a[i+1]))==4 and len(str(a[i+2]))==4)) and  (a[i]%5==0 or a[i+1]%5==0 or a[i+2]%5==0):
+        w=w+1
+        if (a[i]+a[i+1]+a[i+2])>e:
+            e=a[i]+a[i+1]+a[i+2]
+print(w,e)
